ac72208f284220774019977eae5f91d17b085a11
Vladimir Melkonyan <melkonyyan.1998@mail.ru>
2023-05-31 12:58:40 +0300

    Добавил кнопки меню

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 
 # Функция-обработчик команды /start
 def start(update, context):
-    button = KeyboardButton('/time')
-    keyboard = ReplyKeyboardMarkup([[button]], resize_keyboard=True)
+    button_menu = KeyboardButton('/menu')
+    keyboard = ReplyKeyboardMarkup([[button_menu]], resize_keyboard=True)
     context.bot.send_message(chat_id=update.effective_chat.id, text='Привет! Я телеграм бот.', reply_markup=keyboard)
 
 # Функция-обработчик команды /time
@@ -19,6 +19,12 @@ def get_time(update, context):
     moscow_tz = pytz.timezone('Europe/Moscow')
     current_time = datetime.now(moscow_tz).strftime("%Y-%m-%d %H:%M:%S")
     context.bot.send_message(chat_id=update.effective_chat.id, text=f"Текущее московское время: {current_time}")
+
+# Функция-обработчик команды /menu
+def menu(update, context):
+    button_time = KeyboardButton('/time')
+    keyboard = ReplyKeyboardMarkup([[button_time]], resize_keyboard=True)
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Выберите опцию:', reply_markup=keyboard)
 
 # Получаем токен вашего бота
 TOKEN = '6208198607:AAEgUzS6QuESJ3PKV8KzZp6zh8FNNGGzNQ4'
@@ -36,6 +42,10 @@ dispatcher.add_handler(start_handler)
 # Регистрируем обработчик команды /time
 time_handler = CommandHandler('time', get_time)
 dispatcher.add_handler(time_handler)
+
+# Регистрируем обработчик команды /menu
+menu_handler = CommandHandler('menu', menu)
+dispatcher.add_handler(menu_handler)
 
 # Запускаем бота
 updater.start_polling()
